17408808c18ae62588f9d896bdfe928622e51370
Chris DiPiero <frogmaster99@msn.com>
2021-08-01 11:16:29 -0500

    finalized win logic

--- scripts/deckbuilder.js
+++ scripts/deckbuilder.js
@@ -11,8 +11,17 @@ class Card {
 }
 
 class DeckBuilder {
-  constructor(card = makeDeck()) {
-    this.card = card
+  constructor(cards = makeDeck()) {
+    this.cards = cards
+  }
+
+  deckSort(deck) {
+    for( let i = deck.length -1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const  temp = deck[i];
+      deck[i] = deck[j];
+      deck[j] = temp;
+    }
   }
 }
 
--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,50 @@ import { DeckBuilder } from './deckbuilder.js';
 
 let gameOver = false;
 
+// make and shuffle deck
 const theDeck = new DeckBuilder();
-console.log(theDeck.card);
+theDeck.deckSort(theDeck.cards);
+console.log(theDeck.cards);
 
+const drawCard = document.querySelector('#button_draw');
+const computerDeck = theDeck.cards.slice(0, (theDeck.cards.length / 2) );
+const playerDeck = theDeck.cards.slice( (theDeck.cards.length / 2) );
+
+let computerTakenCards = [];
+let playerTakenCards = [];
+let temp = [];
+
+const playRound = function () {
+  if (!computerDeck.length || !playerDeck.length) {
+    let won = () => {
+      if (computerTakenCards.length > playerTakenCards.length) {
+        return "The computer won!"
+      } else if(computerTakenCards.length < playerTakenCards.length) {
+        return "You won!"
+      } else {
+        return "It was a tie!"
+      }
+    }
+    console.log(`game over! ${won()}`)
+  }
+  if (computerDeck[0].value > playerDeck[0].value) {
+    computerTakenCards.push(computerDeck.splice(0, 1), playerDeck.splice(0, 1));
+    if(temp.length) {tempHolding(computerTakenCards)}
+  } else if (computerDeck[0].value > playerDeck[0].value) {
+    PlayerTakenCards.push(computerDeck.splice(0, 1), playerDeck.splice(0, 1));
+    if(temp.length) {tempHolding(playerTakenCards)}
+  } else {
+    temp.push(computerDeck.splice(0, 1), playerDeck.splice(0, 1));
+    playRound();
+  }
+  console.log("computerDeck: " + computerDeck.length);
+}
+
+const tempHolding = function(takenDeck) {
+  takenDeck.push(...temp.splice(0));
+}
+
+drawCard.addEventListener('click', playRound);
 // while (!gameOver) {
 
 // }
