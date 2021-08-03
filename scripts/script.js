595fd7a5c77e88c75bce35ac991ee2f00a37206e
Chris DiPiero <frogmaster99@msn.com>
2021-08-03 02:04:16 -0500

    added scores

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 import { DeckBuilder } from './deckbuilder.js';
 
 let gameOver = false;
+let drawThree = false;
 
 // make and shuffle deck
 const theDeck = new DeckBuilder();
@@ -8,6 +9,9 @@ theDeck.deckSort(theDeck.cards);
 console.log(theDeck.cards);
 
 const drawCard = document.querySelector('#button_draw');
+const compScore = document.querySelector('#comp-score');
+const playScore = document.querySelector('#play-score');
+
 const computerDeck = theDeck.cards.slice(0, (theDeck.cards.length / 2) );
 const playerDeck = theDeck.cards.slice( (theDeck.cards.length / 2) );
 
@@ -38,8 +42,59 @@ const calcFace = function(player, cardData) {
   root.style.setProperty(`--svg-position-${player}`, `${-179.5 * x}px ${-252 * y}px`);
 }
 
+const shootCard = function () {
+
+}
+
+const makeWarNotLove = function () {
+
+}
+
+const removeCards = function () {
+  let computerCard = document.querySelector(".computer-face-card");
+  let playerCard = document.querySelector(".player-face-card");
+  computerCard.classList.add('discard');
+  playerCard.classList.add('discard');
+  setTimeout(function () {
+    computerCard.remove();
+    playerCard.remove();
+    drawCard.disabled = false;
+    compScore.innerHTML = computerTakenCards.length;
+    playScore.innerHTML = playerTakenCards.length;
+  }, 5000);
+}
+
+const endRound = function (takenCards, side) {
+  takenCards.push(computerDeck.splice(0, 1), playerDeck.splice(0, 1)); // pushes won cards to this take pile
+  if(temp.length) {tempHolding(takenCards)} // pulls temp cards (from tie) to this take pile
+  console.log(`The ${side} won.`);
+  removeCards(side);
+}
+
+const tempHolding = function(takenDeck) { //pulls all cards from temp array into winner
+  takenDeck.push(...temp.splice(0));
+}
+
+const createCard = function(side, cardData) {
+  const playArea = document.querySelector(".play_area");
+  const cardFace = document.createElement('img');
+  const thisCard = document.createElement('card');
+
+  calcFace(side, cardData);
+
+  cardFace.classList.add('card', 'face');
+  cardFace.setAttribute('src', '/images/playing_cards.svg')
+  
+  thisCard.classList.add('card_container', `${side}`, `${side}-face-card`);
+
+  thisCard.appendChild(cardFace);
+  playArea.appendChild(thisCard);
+}
+
+
 // war logic
 const playRound = function () {
+  drawCard.disabled = true;
   if (!computerDeck.length || !playerDeck.length) {
     let won = () => {
       if (computerTakenCards.length > playerTakenCards.length) {
@@ -58,38 +113,21 @@ const playRound = function () {
   createCard("player", playerDeck[0]);
   console.log(playerDeck[0]);
 
-  if (computerDeck[0].value > playerDeck[0].value) {
-    computerTakenCards.push(computerDeck.splice(0, 1), playerDeck.splice(0, 1));
-    if(temp.length) {tempHolding(computerTakenCards)}
-  } else if (computerDeck[0].value > playerDeck[0].value) {
-    PlayerTakenCards.push(computerDeck.splice(0, 1), playerDeck.splice(0, 1));
-    if(temp.length) {tempHolding(playerTakenCards)}
-  } else {
-    temp.push(computerDeck.splice(0, 1), playerDeck.splice(0, 1));
+  if (computerDeck[0].value > playerDeck[0].value) { // computer wins round
+    endRound(computerTakenCards, "computer");
+  } 
+  else if (computerDeck[0].value < playerDeck[0].value) { //player wins round
+    endRound(playerTakenCards, "player");
+  } 
+  else { //tie
+    if(drawThree) {
+      makeWarNotLove(); //tests for house rule draw three
+    }
+    temp.push(computerDeck.splice(0, 1), playerDeck.splice(0, 1)); // cards added to temp
     playRound();
   }
 }
 
-const tempHolding = function(takenDeck) { //adds to temp array for ties
-  takenDeck.push(...temp.splice(0));
-}
-
-const createCard = function(player, cardData) {
-
-  const playArea = document.querySelector(".play_area");
-  const cardFace = document.createElement('img');
-  const thisCard = document.createElement('card');
-
-  calcFace(player, cardData);
-
-  cardFace.classList.add('card');
-  cardFace.setAttribute('src', '/images/playing_cards.svg')
-  
-  thisCard.classList.add('card_container', 'face', `${player}`);
-
-  thisCard.appendChild(cardFace);
-  playArea.appendChild(thisCard);
-}
 
 
 
